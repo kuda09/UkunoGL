37dd35e0463320c5d3034277f389720016b647c6
kuda <ku-da@hotmail.co.uk>
2015-10-05 23:28:08 +0100

    navigation implementation

---
+++ resources/js/app/siteNavigation.js
@@ -0,0 +1,58 @@
+define(["jquery"], function ($) {
+
+
+    var self;
+    var ACTIVE_CLASS = 'active';
+    var $context = '.navigation';
+    var nodes = {
+        $menuIcon: $('.mobileMenu', $context),
+        $mobileDropdown: $('.dropdownContainer a', $context)
+    };
+
+
+    var init = function () {
+
+        setEvents();
+    }
+
+    var setEvents = function () {
+
+        nodes.$menuIcon.on('click', handleMobileMenuByClick);
+        nodes.$mobileDropdown.on('click', handleMobileDropdownByClick);
+    }
+
+    var handleMobileMenuByClick = function (e) {
+
+        e.preventDefault();
+
+        var $target = $(this);
+
+        $target
+            .toggleClass(ACTIVE_CLASS)
+            .next()
+            .toggleClass(ACTIVE_CLASS);
+
+    };
+
+    var handleMobileDropdownByClick = function (e) {
+
+        if ($(window).width() < 520){
+
+            e.preventDefault();
+
+            var $target = $(e.target);
+
+            $target
+                .toggleClass(ACTIVE_CLASS)
+                .next()
+                .toggleClass(ACTIVE_CLASS)
+                .parent('.dropdownContainer')
+                .toggleClass(ACTIVE_CLASS);
+        }
+
+
+    }
+
+    return init;
+
+})
