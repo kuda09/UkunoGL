23563caf4c7d6488bfcd8b598a453c65403a4da4
kuda09 <ku-da@hotmail.co.uk>
2015-10-26 00:38:53 +0000

    reverted back to the old table

--- dashboard.php
+++ dashboard.php
@@ -38,8 +38,7 @@ if (!isset($_SESSION['username'])) {
 
     <main role="main" class="dashBoard">
         <div class="container">
-            <h1> Welcome <?php echo $_SESSION['username']; ?> </h1>
-
+            <h1> Welcome <?php echo $_SESSION['username'];?> </h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos iste molestiae odio possimus quidem, tenetur voluptates? Aperiam architecto delectus hic nisi provident, quas saepe vitae? Dicta ratione repellat soluta!</p>
         </div>
         <div class="container">
@@ -54,28 +53,28 @@ if (!isset($_SESSION['username'])) {
 
                 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="createNewOrder" method="post">
 
-                    <label for="orderName"> Name</label>
+                    <label for="orderName">Order Name</label>
                     <input type="text" placeholder="Order Name" id="orderName" name="orderName">
 
-                    <label for="orderSender"> Sender</label>
+                    <label for="orderSender">Order Sender</label>
                     <input type="text" placeholder="Order Sender" id="orderSender" name="orderSender">
 
-                    <label for="orderPickup"> Pickup Place</label>
+                    <label for="orderPickup">Order Pickup Place</label>
                     <input type="text" placeholder="Order Pickup Place" id="orderPickup" name="orderPickup">
 
-                    <label for="orderDestination"> Destination</label>
+                    <label for="orderDestination">Order Destination</label>
                     <input type="text" placeholder="Order Destination" id="orderDestination" name="orderDestination">
 
-                    <label for="orderArrival"> Arrival</label>
+                    <label for="orderArrival">Order Arrival</label>
                     <input type="date" placeholder="Order Arrival" id="orderArrival" name="orderArrival">
 
-                    <label for="orderPrice"> Price</label>
+                    <label for="orderPrice">Order Price</label>
                     <input type="text" placeholder="$9.00" id="orderPrice" name="orderPrice">
 
-                    <label for="orderProgress"> Progress <span>The order progess must be a number between 0 and 100*</span></label>
+                    <label for="orderProgress">Order Progress <span>The order progess must be a number between 0 and 100*</span></label>
                     <input type="text" placeholder="10" id="orderProgress" name="orderProgress">
 
-                    <label for="orderDetails"> Details</label>
+                    <label for="orderDetails">Order Details</label>
                     <textarea name="orderDetails" id="orderDetails" cols="30" rows="10"></textarea>
 
 
@@ -99,17 +98,11 @@ if (!isset($_SESSION['username'])) {
                     if ($_SERVER['REQUEST_METHOD'] == "POST") :
 
                         if ($connect->query($query) == TRUE) :
+
+
                             ?>
                             <div class="success">
-                                <?php
-
-                                echo "You have created a new order successfully" . "<br>";
-
-                                echo "<p>" . "Your order id is " . $order_id . "</p>";
-
-                                ?>
-
-                                <a href="update-order.php?id=<?php echo $order_id; ?>" class="btn btnPrimary">view your order</a>
+                                <?php echo "You have created a new order successfully"; ?>
                             </div>
                         <?php else: ?>
                             <div class="error">
@@ -145,39 +138,40 @@ if (!isset($_SESSION['username'])) {
             <div class="col">
                 <h2>update an order status</h2>
 
-                <?php if (!$row) : ?>
+                <table class="table table-responsive table-bordered updateOrder">
+                    <thead>
+                    <tr>
+                        <td>Order Name</td>
+                        <td>Update</td>
+                    </tr>
+                    </thead>
+                    <tbody>
 
-                    <p>No current orders in the database</p>
+                    <?php
 
-                <?php else:?>
-                    <table class="table table-responsive table-bordered updateOrder">
-                        <thead>
+                    while ($row = mysqli_fetch_assoc($select)) {
+                        ?>
                         <tr>
-                            <td>Order Name</td>
-                            <td>Update</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php while ($row = mysqli_fetch_assoc($select)) { ?>
-                            <tr>
-                                <td>
-                                    <?php echo $row['order_name']; ?>
-                                </td>
-                                <td>
-                                    <form action="update-order.php" method="get">
+                            <td>
+                                <?php echo $row['order_name'];?>
+                            </td>
+                            <td>
+                                <form action="update-order.php" method="get">
 
-                                        <input type="hidden" value="<?php echo $row['order_id']; ?>" name="id">
-                                        <input type="hidden" value="<?php echo $row['order_name']; ?>" name="name">
-                                        <button class="btn btnSecondary" type="submit">
-                                            View or Update Order
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
-                <?php endif; ?>
+                                    <input type="hidden" value="<?php echo $row['order_id']; ?>" name="id">
+                                    <input type="hidden" value="<?php echo $row['order_name'] ;?>" name="name">
+                                    <button class="btn btnSecondary" type="submit">
+                                        View or Update Order
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                </table>
             </div>
         </div>
 
